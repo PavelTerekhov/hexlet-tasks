73d346dddbff53263f870cad838a3a7b9906ca80
Pavel <pavel.a.terekhov@gmail.com>
2022-08-03 11:51:31 +0700

    Task 17

--- hexlet-task.js
+++ hexlet-task.js
@@ -664,3 +664,33 @@ export default numberSumm;
 /* eslint-disable import/prefer-default-export */
 export const length = (str) => str.length;
 
+
+
+// Task 17 
+
+// solution.js
+// Реализуйте и экспортируйте по умолчанию функцию, которая делает заглавной первую букву каждого слова в предложении.
+
+// Пример
+// solution('hello, world!'); // Hello, World!
+// Подсказки
+// Вычисление длины строки: length(str).
+// Перевод строки/буквы в верхний регистр: toUpperCase(str).
+
+// @ts-check
+
+import { length, toUpperCase } from './strings.js';
+
+// BEGIN (write your solution here)
+export default (str) => {
+  let result = "";
+  for (let i = 0; i < length(str); i++) {
+    const shouldBeBig = str[i] !== " " && (i === 0 || str[i - 1] === " ");
+    result += shouldBeBig ? toUpperCase(str[i]) : str[i];
+  }
+
+  return result;
+}
+// END
+
+
