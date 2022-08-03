1b1ae0ca58ee62340453cba24eb585edaca08362
Pavel <pavel.a.terekhov@gmail.com>
2022-08-03 14:55:14 +0700

    Task 2 for Git

--- hexlet-task.js
+++ hexlet-task.js
@@ -694,3 +694,19 @@ export default (str) => {
 // END
 
 
+
+// Курс по Git 
+
+
+// # При выполнении задачи состояние репозитория могло измениться.
+// # Перед набором команд из списка, обновите упражнение с помощью кнопки Сброс.
+
+// git clone repos/git-user code-user
+// cd code-user/
+// echo 'I like to change files' >> hexlet.txt
+// echo '<h1>С помощью гит можно писать книги</h1>' > index.html
+// git add .
+// git config --global user.email "you@example.com"
+// git config --global user.name "Your Name"
+// git commit -m 'fix hexlet.txt and index.html'
+// git push
